db7885a19dc078b305f188930626a8a7e222a3c4
Jamie Goodnight <goodnightlambda@gmail.com>
2019-04-01 16:09:52 -0400

    Everything working! MVP

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-const db = require("./users/users-model.js");
+const db = require("./users/usersModel.js");
 
 server.post("/api/register", (req, res) => {
   const user = req.body;
@@ -69,7 +69,27 @@ server.get("/api/users", restricted, (req, res) => {
     });
 });
 
-const restricted = () => {};
+function restricted(req, res, next) {
+  const { username, password } = req.body;
+  if (username && password) {
+    db.getUserByName({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid username or password!" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "There was a problem logging in!" });
+      });
+  } else {
+    res
+      .status(401)
+      .json({ message: "Please provide a username and password to login!" });
+  }
+}
 
 server.listen(5000, () => {
   console.log("\n*** Server Running on http://localhost:5000 ***\n");
