31d2e75267dea0c5fbc389a147f1b08db7dd7c9b
Jamie Goodnight <goodnightlambda@gmail.com>
2019-04-02 15:16:23 -0400

    MVP! This is one secure server.

--- index.js
+++ index.js
@@ -59,12 +59,13 @@ server.post("/api/register", (req, res) => {
 
 server.post("/api/login", (req, res) => {
   const { username, password } = req.body;
+
   if (username && password) {
     db.getUserByName({ username })
       .then(user => {
         if (bcrypt.compareSync(password, user.password)) {
+          req.session.user = user;
           res.status(200).json({ message: "LOGGED IN" });
-          next();
         } else {
           res.status(401).json({ message: "Invalid username or password!" });
         }
@@ -92,26 +93,34 @@ server.get("/api/users", restricted, (req, res) => {
 });
 
 function restricted(req, res, next) {
-  const { username, password } = req.body;
-  if (username && password) {
-    db.getUserByName({ username })
-      .then(user => {
-        if (bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ message: "There was a problem logging in!" });
-      });
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res
-      .status(401)
-      .json({ message: "Please provide a username and password to login!" });
+    res.status(400).json({ message: "Invalid credentials!" });
   }
 }
 
 server.listen(5000, () => {
   console.log("\n*** Server Running on http://localhost:5000 ***\n");
 });
+
+/* Restricted function without sessions */
+
+// const { username, password } = req.body;
+// if (username && password) {
+//   db.getUserByName({ username })
+//     .then(user => {
+//       if (bcrypt.compareSync(password, user.password)) {
+//         next();
+//       } else {
+//         res.status(401).json({ message: "You shall not pass!" });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: "There was a problem logging in!" });
+//     });
+// } else {
+//   res
+//     .status(401)
+//     .json({ message: "Please provide a username and password to login!" });
+// }
